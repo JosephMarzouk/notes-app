d1648bda0fa845dd2ced0bda7c0f8808f83982fc
JosephMarzouk <joseph1marzok@gmail.com>
2024-01-30 18:36:36 +0200

    appbar added

--- lib/view/homeScreen.dart
+++ lib/view/homeScreen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/customsearchicon.dart';
+import 'package:notes_app/widgets/homeviewbody.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        forceMaterialTransparency: true,
+        toolbarHeight: 70,
+        title: const Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Text(
+            'Notes',
+            style: TextStyle(fontSize: 25),
+          ),
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(top: 17, right: 13),
+            child: CustomSearchIcon(),
+          )
+        ],
+      ),
+      body: const HomeViewBody(),
+    );
   }
 }
