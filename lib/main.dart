026a74237a29f408f6175a5a1768f039114c15e5
JosephMarzouk <joseph1marzok@gmail.com>
2024-02-03 16:08:32 +0200

    add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/view/homeScreen.dart';
 
 void main() async{
   await Hive.initFlutter();
   Hive.openBox(KNotesBox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
 
